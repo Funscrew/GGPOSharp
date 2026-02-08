0a0da03ef9597ca0e1d6345efdf1e543b1c458f8
Andrew A. Ritz <drew@august-harper.com>
2026-02-08 12:49:19 -0500

    - Fixed a bug in the sim network re: rollbacks.  Both clients were using the same callback structure, and that was causing an explosion.

--- GGPOSharpTesters/ReplayApplianceTesters.cs
+++ GGPOSharpTesters/ReplayApplianceTesters.cs
@@ -1,3 +1,4 @@
+using drewCo.Tools.Logging;
 using GGPOSharp;
 using GGPOSharp.Clients;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
@@ -21,19 +22,18 @@ namespace GGPOSharpTesters
     {
     }
 
+    //// --------------------------------------------------------------------------------------------------------------------------
+    //private static void NoOp_Rollback(int flags)
+    //{
+    //  // TODO: Logging...
+    //  GGPOSharp.Program.RunFrame(Client, TestInput);
+    //}
+
     // --------------------------------------------------------------------------------------------------------------------------
-    private unsafe static bool NoOp(byte* arg)
+    private static void NoOp_BeginGame(string gameName)
     {
-      // TODO: Logging...
-      return true;
+      Log.Debug($"The game: {gameName} was started!");
     }
-    // --------------------------------------------------------------------------------------------------------------------------
-    private static void NoOp(int flags)
-    {
-      // TODO: Logging...
-    }
-    // --------------------------------------------------------------------------------------------------------------------------
-    private static void NoOp_BeginGame(string gameName) { }
 
     // --------------------------------------------------------------------------------------------------------------------------
     private static bool NoOp_Event(ref GGPOEvent evt)
@@ -44,12 +44,25 @@ namespace GGPOSharpTesters
     // --------------------------------------------------------------------------------------------------------------------------
     private unsafe static bool NoOp_SaveGame(byte** buffer, int* len, int* checksum, int frame)
     {
+      // We need to have some kind of data to save, or the system will explode!
+      *buffer = (byte*)0x1;
+      *len = 1;
+      *checksum = 0;
+
       return true;
     }
 
     // --------------------------------------------------------------------------------------------------------------------------
     private unsafe static bool NoOp_LoadGame(byte** buffer, int len)
     {
+      return true;
+    }
+
+    // ------------------------------------------------------------------------------------------------------
+    private static unsafe bool NoOp_FreeBuffer(byte* arg)
+    {
+      // NOTE: We don't have to do anything here!
+      //Log.Info("An indication to free a buffer happened!");
       return true;
     }
 
@@ -86,19 +99,6 @@ namespace GGPOSharpTesters
       // I may want to look into a better way to setup the appliance + its clients so that there
       // are less options, etc. flying all over the place.
       // throw new NotImplementedException();
-      var callbacks = new GGPOSessionCallbacks()
-      {
-        free_buffer = NoOp,
-        rollback_frame = NoOp,
-        begin_game = NoOp_BeginGame,
-        // one day....
-        //on_event = (ref x) => {
-        //  return true;
-        //}
-        on_event = NoOp_Event,
-        save_game_state = NoOp_SaveGame,
-        load_game_state = NoOp_LoadGame
-      };
 
       //// This is the replay appliance.  It accepts connections from one of the normal clients.
       //var applianceOps = new GGPOClientOptions(GGPOConsts.REPLAY_APPLIANCE_PLAYER_INDEX, RA_PORT, Defaults.PROTOCOL_VERSION, SESSION_ID);
@@ -119,43 +119,61 @@ namespace GGPOSharpTesters
       const string PLAYER1_HOST = "127.0.0.1";
       const string PLAYER2_HOST = "192.168.1.3";
 
+      byte[] p1Input = new byte[5 * MAX_PLAYERS];
+      byte[] p2Input = new byte[5 * MAX_PLAYERS];
+
+
       var p1Udp = new SimUdp(PLAYER1_HOST, PLAYER1_PORT, timeSource, testQueue, SIM_PING, SIM_JITTER);
       var p1Ops = new GGPOClientOptions(PLAYER1_INDEX, PLAYER1_PORT, Defaults.PROTOCOL_VERSION, SESSION_ID);
       p1Ops.IdleTimeout = 0;
-      p1Ops.Callbacks = callbacks;
-
+      p1Ops.Callbacks = CreateCallbacks(PLAYER1_INDEX);
       var p1GGPO = new GGPOClient(p1Ops, p1Udp, timeSource);
       p1GGPO.AddLocalPlayer("Joe", PLAYER1_INDEX);
+
+      p1Ops.Callbacks.rollback_frame = x =>
+      {
+        GGPOSharp.Program.RunFrame(p1GGPO, p1Input);
+      };
 
 
       var p2Udp = new SimUdp(PLAYER2_HOST, PLAYER2_PORT, timeSource, testQueue, SIM_PING, SIM_JITTER);
       var p2Ops = new GGPOClientOptions(PLAYER2_INDEX, PLAYER2_PORT, Defaults.PROTOCOL_VERSION, SESSION_ID);
       p2Ops.IdleTimeout = 0;
-      p2Ops.Callbacks = callbacks;
+      p2Ops.Callbacks = CreateCallbacks(PLAYER2_INDEX);
 
       var p2GGPO = new GGPOClient(p2Ops, p2Udp, timeSource);
       p2GGPO.AddLocalPlayer("Archie", PLAYER2_INDEX);
+
+      p2Ops.Callbacks.rollback_frame = x =>
+      {
+        GGPOSharp.Program.RunFrame(p2GGPO, p2Input);
+      };
 
 
       // Add the remotes to the test clients:
       p1GGPO.AddRemotePlayer(new RemoteEndpointData(PLAYER2_HOST, PLAYER2_PORT, PLAYER2_INDEX + 1));
       p2GGPO.AddRemotePlayer(new RemoteEndpointData(PLAYER1_HOST, PLAYER1_PORT, PLAYER1_INDEX + 1));
 
+      // The clients need to start with an idle frame....
+      // --> TODO: If this is really a requirement, then it should be done during setup.  Maybe a 'lock' type function, or preload the frame data?
+      //p1GGPO.Idle();
+      //p2GGPO.Idle();
+
       const int TIME_INTERVAL = 1;
       const int FRAME_INTERVAL = 16;
 
       // The total number of 'frames' that we want to simulate in this case.
       const int MAX_FRAMES = 100;
-      for (int i = 1; i < MAX_FRAMES; i++)
+      for (int i = 0; i < MAX_FRAMES; i++)
       {
         timeSource.AddTime(TIME_INTERVAL);
 
         if (i % FRAME_INTERVAL == 0)
         {
-          byte[] p1Input = new byte[5 * MAX_PLAYERS];
-          byte[] p2Input = new byte[5 * MAX_PLAYERS];
+          // TODO: I want to change the inputs per frame.  Data doesn't matter, just that it can be exchanged.
+          // Probably just increment the bits....
+          p1Input[0] = (byte)(i & 0xFF);
 
-          // TODO: A proper RunFrams() function..... (see Program.cs for example)
           GGPOSharp.Program.RunFrame(p1GGPO, p1Input);
           GGPOSharp.Program.RunFrame(p2GGPO, p2Input);
         }
@@ -182,6 +200,24 @@ namespace GGPOSharpTesters
       // Maybe we can capture some events or something?
     }
 
+    // --------------------------------------------------------------------------------------------------------------------------
+    private unsafe GGPOSessionCallbacks CreateCallbacks(byte playerIndex)
+    {
+      var callbacks = new GGPOSessionCallbacks()
+      {
+        free_buffer = NoOp_FreeBuffer,
+        // rollback_frame = NoOp_Rollback,
+        begin_game = NoOp_BeginGame,
+        // one day....
+        //on_event = (ref x) => {
+        //  return true;
+        //}
+        on_event = NoOp_Event,
+        save_game_state = NoOp_SaveGame,
+        load_game_state = NoOp_LoadGame,
+      };
 
+      return callbacks;
+    }
   }
 }
